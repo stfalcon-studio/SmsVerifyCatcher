bdcf8c5d327671755bd1854b813bc6e2cf3a342c
Anton <anton.bevza@stfalcon.com>
2017-03-02 10:33:31 +0200

    replaced deprecated api methods for API 23

--- smsverifycatcher/src/main/java/com/stfalcon/smsverifycatcher/SmsReceiver.java
+++ smsverifycatcher/src/main/java/com/stfalcon/smsverifycatcher/SmsReceiver.java
@@ -1,12 +1,12 @@
 /*******************************************************************************
  * Copyright 2016 Anton Bevza stfalcon.com
- *
+ * <p>
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *
+ * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
- *
+ * <p>
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,13 +19,10 @@ package com.stfalcon.smsverifycatcher;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.telephony.SmsManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 class SmsReceiver extends BroadcastReceiver {
     private OnSmsCatchListener<String> callback;
@@ -47,7 +44,7 @@ class SmsReceiver extends BroadcastReceiver {
             if (bundle != null) {
                 final Object[] pdusObj = (Object[]) bundle.get("pdus");
                 for (int i = 0; i < pdusObj.length; i++) {
-                    SmsMessage currentMessage = SmsMessage.createFromPdu((byte[]) pdusObj[i]);
+                    SmsMessage currentMessage = getIncomingMessage(pdusObj[i], bundle);
                     String phoneNumber = currentMessage.getDisplayOriginatingAddress();
 
                     if (phoneNumberFilter != null && !phoneNumber.equals(phoneNumberFilter)) {
@@ -67,6 +64,17 @@ class SmsReceiver extends BroadcastReceiver {
         } catch (Exception e) {
             Log.e("SmsReceiver", "Exception smsReceiver" + e);
         }
+    }
+
+    private SmsMessage getIncomingMessage(Object aObject, Bundle bundle) {
+        SmsMessage currentSMS;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            String format = bundle.getString("format");
+            currentSMS = SmsMessage.createFromPdu((byte[]) aObject, format);
+        } else {
+            currentSMS = SmsMessage.createFromPdu((byte[]) aObject);
+        }
+        return currentSMS;
     }
 
     /**
